18ca651229b3d43223199de80df2258e027c8b6c
Claire-Lavigne <lavigneclaire81@gmail.com>
2020-06-20 21:47:29 +0200

    Connecting API to MongoDB cluster

--- backend/app-sample.js
+++ backend/app-sample.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('<mongoDB srv address with ID and PASSWORD>',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 // CORS
 app.use((req, res, next) => {
